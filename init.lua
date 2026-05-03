e26b45ff8086304b6c327a5ce9aaaee7c9040189
majockbim <majock.bim1@gmail.com>
2026-05-02 22:46:34 -0400

    tree sitter for styling

--- init.lua
+++ init.lua
@@ -1,9 +1,59 @@
+-- line #'s
 vim.opt.number = true
 
+-- target directory
 if vim.fn.argc() == 0 then
     vim.api.nvim_set_current_dir("C:/Users/pretb/code")
 end
 
+-- cursort effect (neovide)
 if vim.g.neovide then
     vim.g.neovide_cursor_vfx_mode = "railgun" 
 end
+
+-- syntax highlighting
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight-night]])
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "cpp", "go", "lua", "vim", "vimdoc",
+			     "python", "javascript", "html", "css", "typescript", "tsx"},
+        
+        sync_install = false,
+
+        auto_install = true,
+
+        highlight = {
+          enable = true, 
+	  additional_vim_regex_highlighting = false,
+        },
+      })
+    end
+  }
+
+})
