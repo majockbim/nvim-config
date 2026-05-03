112a843495520b145a9709831bb27b93faa4003b
majockbim <majock.bim1@gmail.com>
2026-05-02 22:22:53 -0400

    railgun cursor effect (neovide)

--- init.lua
+++ init.lua
@@ -3,3 +3,7 @@ vim.opt.number = true
 if vim.fn.argc() == 0 then
     vim.api.nvim_set_current_dir("C:/Users/pretb/code")
 end
+
+if vim.g.neovide then
+    vim.g.neovide_cursor_vfx_mode = "railgun" 
+end
